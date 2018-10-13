b122b23475cecc22ee4d42e7399c455a44494293
catsigma <im@catsigma.com>
2018-10-13 12:30:57 +0800

    add install logic

--- src/liqpack.ml
+++ src/liqpack.ml
@@ -1,18 +1,21 @@
 open Sexplib
 
 let (#<) = Printf.sprintf
+exception Error of string
 
 type config = {
   path: string;
   libs: (string, string) Hashtbl.t;  
 }
 
-let init_config = "
-(
-  (path nil)
-  (libs ())
-)
-"
+let construct_config path libs = 
+  Sexp.List [
+    Sexp.List [Sexp.Atom "path"; Sexp.Atom path;];
+    Sexp.List [Sexp.Atom "libs"; Sexp.List libs;];
+  ]
+
+let init_config = 
+  construct_config "nil" []
 
 let parse_config config_raw =
   let rec loop sexp result =
@@ -29,6 +32,9 @@ let parse_config config_raw =
   in
   loop config_raw {path = ""; libs = Hashtbl.create 256;}
 
+let gen_config config =
+  construct_config config.path 
+    (Hashtbl.fold (fun k v acc -> Sexp.List [Sexp.Atom k; Sexp.Atom v] :: acc) config.libs [])
 
 let write_config sexp =
   Sexp.save_hum "liqpack.cfg" sexp
@@ -37,12 +43,26 @@ let read_config () =
   try
     Sexp.load_sexp "liqpack.cfg"
   with Sys_error _ ->
-    write_config (Sexp.of_string init_config);
+    write_config init_config;
     Sexp.load_sexp "liqpack.cfg"
 
+let git_clone name url =
+  Sys.command ("git clone " ^ url ^ " libs/" ^ name)
 
 let sys_arg_parse config =
   match Sys.argv with
+  | [| _ ; "install" ; url |] -> 
+    if Str.search_forward (Str.regexp "\\([a-zA-Z0-9_\\-]+\\)\\.git") url 0 > 0 then
+      let name = Str.matched_group 1 url in
+      if git_clone name url <> 0 then
+        raise (Error "git clone failed")
+      else
+        Hashtbl.add config.libs name url;
+        gen_config config |> write_config
+    else
+      raise (Error "invalid git url: %s" #< url)
+
+  | [| _ ; "setpath" ; arg |] -> gen_config {config with path = arg} |> write_config
   | [| _ ; command ; arg |] -> print_endline ("good command" ^ command ^ arg)
   | [| _ ; command |] -> print_endline ("bad command" ^ command)
   | _ -> print_endline "
